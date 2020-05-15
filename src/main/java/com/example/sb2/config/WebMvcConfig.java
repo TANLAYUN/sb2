78a9e23799e428e13564993b41303e319ecf98da
谈腊云 <2462289249@qq.com>
2020-05-15 13:31:23 +0800

    5.15

---
+++ src/main/java/com/example/sb2/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.example.sb2.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${prop.upload-folder}")
+    private String UPLOAD_FOLDER;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //文件磁盘图片url 映射
+        //配置server虚拟路径，handler为前台访问的目录，locations为files相对应的本地路径
+        registry.addResourceHandler("/static/**").addResourceLocations("file:"+UPLOAD_FOLDER);
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
+
+}
+
+
